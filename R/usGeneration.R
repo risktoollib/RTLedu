044a232de995d62d86af2398aa8b231a5046ebea
risktoollib <pcote@ualberta.ca>
2023-02-05 18:03:22 -0700

    usGen

--- R/usGeneration.R
+++ R/usGeneration.R
@@ -1,6 +1,5 @@
 #' Produce a clean analysis on the US Electricity Market
 #' @description Assignment using data from the Energy Information Administration (EIA) API.
-#' Your task is to perform analysis and render it as effective business communication.
 #' @param name Tutorial name
 #' @return 'learnr' module
 #' @export usGeneration
@@ -12,5 +11,4 @@
 
 usGeneration <- function(name = "usGeneration") {
   learnr::run_tutorial(name = name, package = "RTLedu")
-  #quarto::quarto_run("inst/tutorials/usGeneration/usGeneration.qmd", render = FALSE)
 }
